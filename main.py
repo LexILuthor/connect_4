6c259c0a012fb1515891fa0b963465148791f534
francesco chini <francesco.chini1990@gmail.com>
2021-03-14 10:10:45 +0100

    is_winning()  is broken
    I have modified the matrix in main.py in order to test is_winning() on a new example, but if I run main.py I get an error message.

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def testingmyfunction():
     matrix = np.array(
         [[0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 1],
-         [0, 0, 0,-1, 1, 1],
-         [0, 0,-1, 1, 1,-1],
-         [0,-1, 1,-1,-1, 1]
+         [0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0],
+         [0, -1, 0, 0, 0, 0],
+         [1, 1, 1, 1,-1, -1]
          ])
 
-    last_move_column = 4
+    last_move_column = 0
     result = myFun.is_winning(matrix, last_move_column, empty=0, red=-1, yellow=1)
     print("the result is " + str(result))
 
