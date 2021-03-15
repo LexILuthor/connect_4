02dcc8d18939b93bd19b7bc2244a216d6a28dc00
LexILuthor <spaziotemporale.com@gmail.com>
2021-03-16 00:09:23 +0100

    quick fix

--- neural_network.py
+++ neural_network.py
@@ -2,13 +2,12 @@ import mnist
 import numpy as np
 from tensorflow import keras
 
-
 # Flatten the matrix.
 my_train_matrix = train_matrix.flatten()
 
 # Build the model.
 model = Sequential([
-  Dense(64, activation='relu', input_shape=(784,)),
-  Dense(64, activation='relu'),
-  Dense(10, activation='softmax'),
+    Dense(64, activation='relu', input_shape=(len(my_train_matrix),)),
+    Dense(64, activation='relu'),
+    Dense(1, activation='softmax'),
 ])
