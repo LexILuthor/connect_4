b83575f4695d7a69f347fccbf32008a7ce58f084
LexILuthor <spaziotemporale.com@gmail.com>
2021-03-16 11:34:37 +0100

    quick fix

--- myFunctions.py
+++ myFunctions.py
@@ -4,36 +4,7 @@ import matplotlib.pyplot as plt
 import secondary_Functions as secFun
 
 
-def print_board(board, empty=0, red=-1):
-    # yellow = 1
-    empty_x = []
-    empty_y = []
-    red_x = []
-    red_y = []
-    yellow_x = []
-    yellow_y = []
-    for r in range(len(board)):
-        for c in range(len(board[0])):
-            if board[r][c] == empty:
-                empty_x.append(int(c))
-                empty_y.append(-int(r))
-            elif board[r][c] == red:
-                red_x.append(int(c))
-                red_y.append(-int(r))
-            else:
-                yellow_x.append(int(c))
-                yellow_y.append(-int(r))
 
-    point_dimension = 20
-    shape_of_points = "H"
-    fig, ax = plt.subplots()
-    ax.set_xlim([-1, len(board[0])])
-    ax.set_ylim(([-len(board), 1]))
-
-    ax.scatter(empty_x, empty_y, color='grey', marker=shape_of_points, alpha=0.5, linewidths=point_dimension)
-    ax.scatter(red_x, red_y, color='red', marker=shape_of_points, linewidths=point_dimension)
-    ax.scatter(yellow_x, yellow_y, color='yellow', marker=shape_of_points, linewidths=point_dimension)
-    fig.show()
 
 
 #----------------------------------------------------------------------------------------------------------
@@ -111,3 +82,36 @@ def play_a_game(Q, epsilon=0.1, number_of_rows=6, number_of_columns=7,
     # --------------------------------------------------------------------------------------------------------------
 
     return SA_intermediate_state, r, S_prime
+
+
+
+def print_board(board, empty=0, red=-1):
+    # yellow = 1
+    empty_x = []
+    empty_y = []
+    red_x = []
+    red_y = []
+    yellow_x = []
+    yellow_y = []
+    for r in range(len(board)):
+        for c in range(len(board[0])):
+            if board[r][c] == empty:
+                empty_x.append(int(c))
+                empty_y.append(-int(r))
+            elif board[r][c] == red:
+                red_x.append(int(c))
+                red_y.append(-int(r))
+            else:
+                yellow_x.append(int(c))
+                yellow_y.append(-int(r))
+
+    point_dimension = 20
+    shape_of_points = "H"
+    fig, ax = plt.subplots()
+    ax.set_xlim([-1, len(board[0])])
+    ax.set_ylim(([-len(board), 1]))
+
+    ax.scatter(empty_x, empty_y, color='grey', marker=shape_of_points, alpha=0.5, linewidths=point_dimension)
+    ax.scatter(red_x, red_y, color='red', marker=shape_of_points, linewidths=point_dimension)
+    ax.scatter(yellow_x, yellow_y, color='yellow', marker=shape_of_points, linewidths=point_dimension)
+    fig.show()
