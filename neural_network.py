c82c74791f07e3ffcb0f3b83bc18ef79a1c2e947
francesco chini <francesco.chini1990@gmail.com>
2021-03-16 15:00:58 +0100

    I just worked a little bit on the NN

--- neural_network.py
+++ neural_network.py
@@ -2,11 +2,12 @@
 # Here I just copy-pasted a CNN written for MNIST as personal exercise
 # I am currently in the process of making it useful for connect 4
 # !!! THIS IS WORK IN PROGRESS !!!
-
-
 import numpy as np
 import tensorflow as tf 
-from tensorflow.keras import layers, models, losses
+from tensorflow.keras import layers,  losses
+
+# !!! WARNING !!! The number of actions need to be loaded and this will be loaded
+num_actions = 10
 
 
 # Download the data MNIST (hand written digits)
@@ -20,7 +21,7 @@ x_test = x_test.reshape(-1, rows, columns,1)
 
 
 # Create the CNN with keras
-model = models.Sequential([
+model = tf.keras.Sequential([
   layers.Conv2D( 28, (3, 3), activation='relu', input_shape= (rows,columns,1)),  
   #layers.MaxPooling2D((2,2)),            #  NOTE: Probably we will not need to do any pooling for connect 4 
   # the following layer serves to prevent overfitting by dropping out neurons randomly during training
@@ -31,7 +32,7 @@ model = models.Sequential([
   # the following layer serves to prevent overfitting by dropping out neurons randomly during training
   layers.Dropout(0.2),  
   # the last output layer is also dense 
-  layers.Dense(10)  			# -------------------------- !!! WARNING !!!  Instead of 10 we will use the number of actions
+  layers.Dense(num_actions)  			# -------------------------- !!! WARNING !!!  Instead of 10 we will use the number of actions
 ])
 
 
@@ -40,15 +41,15 @@ model = models.Sequential([
 loss_fn = losses.SparseCategoricalCrossentropy(from_logits=True)
 
 # Now we set the training configuration
-model.compile(optimizer='SGD', 			# adam is a type of stochastic gradient descent 
-              loss=loss_fn,					# the loss is our loss function 
-              metrics=['accuracy'])
+#model.compile(optimizer='adam', 			# adam is a type of stochastic gradient descent 
+#              loss=loss_fn,					# the loss is our loss function 
+#              metrics=['accuracy'])
 
 # Let us train the model
-model.fit(x_train, y_train, epochs=3)		# epochs are the numnbers of trainings (each train goes through the entire trainins set)
-model.evaluate(x_test,  y_test, verbose=2)	# let's evaluate the performance
+#model.fit(x_train, y_train, epochs=1)		# epochs are the numnbers of trainings (each train goes through the entire trainins set)
+#model.evaluate(x_test,  y_test, verbose=2)	# let's evaluate the performance
 
 
-
-
+predictions = model.predict(x_train)
+print(predictions[0])
 
