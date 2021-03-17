57046ac8fc83819eace5a20276715a787be1dc3b
francesco chini <francesco.chini1990@gmail.com>
2021-03-17 13:25:55 +0100

    Still work in progress with training NN

--- neural_network.py
+++ neural_network.py
@@ -5,6 +5,7 @@
 import numpy as np
 import tensorflow as tf 
 from tensorflow.keras import layers,  losses
+import tensorflow.keras.backend as kb
 
 # a function that returns our initialized neural network
 def create_NN(n_rows, n_columns):
@@ -22,6 +23,7 @@ def create_NN(n_rows, n_columns):
 # Q_eval is a function that given our neural network Q and the current state s returns the values of all actions
 def Q_eval(Q, current_state):
     # we need to reshape the state into a tensor
+    current_state = np.array(current_state)
     (n_rows, n_columns) = np.shape(current_state)
     current_state = current_state.reshape(1, n_rows, n_columns, 1)
     return Q.predict(current_state)
@@ -31,9 +33,50 @@ def Q_eval(Q, current_state):
 # To Do List
 
 
-# a function that given a batch of training set coming from memory D, computes
-# the target value, and then trains the neural network 
-def train_my_NN(experience_batch, Q):
+# a function that given a batch of training set coming from memory D, and the discount rate gamma
+# computes the target value, and then trains the neural network 
+# !!! WARNING !!!
+# I expect experience_batch to be a list with N elements and each element is a 4-list
+def train_my_NN(experience_batch, Q, gamma):
+	# lenght of experience batch
+	len_batch = len(experience_batch)
 
-    pass
+	# initialize target
+	y = np.zeros(len_batch)
+	# initialize prediction
+	y_pred = np.zeros(len_batch)
+	for i in range(len_batch):
+		# the first element in each 4-list is the state s
+		s = experience_batch[i][0]
+		# the second element is the action a
+		a = experience_batch[i][1]
+		# let's compute the predicted value of (s,a)
+		y_pred[i] = Q_eval(Q, s)[a]
+
+		# the third element in each 4-list is the reward
+		r = experience_batch[i][2]
+		# the 4th element is the state s'
+		s_prime = experience_batch[i][3]
+		# case when s' is not terminal (!!! WARNING !!! I assume that this happens iff r == 0)
+		if r == 0:
+			Q_val = Q_eval(Q, s_prime)
+			y[i] = r + gamma * np.max(Q_val)
+		# case when s' is terminal
+		else:
+			y[i] = r
+
+	# define the loss function
+	error = (y - y_pred)
+	loss = np.inner(error,error)
+
+	# Now we set the training configuration
+	Q.compile(optimizer='adam', 			# adam is a type of stochastic gradient descent 
+              loss=loss,					# the loss is our loss function 
+              metrics=['accuracy'])
+
+	# train
+	Q
+
+		
+    
 
