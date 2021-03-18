666b36d5e0d2d215fcef2c02cb3c32431cf45107
francesco chini <francesco.chini1990@gmail.com>
2021-03-18 13:52:00 +0100

    I have implemented an untrained NN. Check the main. Very weirdgit add . The agent NEVER LOSE?!

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import neural_network as nn
 import copy
 
 def main():
-    number_of_moves = 100
+    number_of_moves = 1000
     memory_size = 10  # Let's set the memory capacity
     # Initialize memory
     memory = []
@@ -16,9 +16,18 @@ def main():
     # Initialize first state
     S = np.copy(board)
     
+    # FOR DEBUGGING
+    count_win = 0
+    count_lose = 0
     # let's fill up the memory first
     for move in range(number_of_moves):
         (S, r, a, S_prime) = copy.deepcopy(myFun.play_move(Q, S, epsilon=0.2))
+
+        # THIS IS FOR DEBUGGIN 
+        if r == 1:
+            count_win += 1
+        if r == -1:
+            count_lose -= 1
         # if memory is still no full
         if len(memory) < memory_size:
             # add experience to memory
@@ -35,9 +44,8 @@ def main():
 
 
 
-    print(memory)
-    print(memory[3])
-    print(memory[4])
+    print(count_win)
+    print(count_lose)
 
 if __name__ == '__main__':
     main()
