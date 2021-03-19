87274f056c78dd30407010576128e7887bf685b2
francesco chini <francesco.chini1990@gmail.com>
2021-03-19 12:15:27 +0100

    added few line of code in main to test the AI

--- main.py
+++ main.py
@@ -62,9 +62,34 @@ def main():
     # Save the weights
     Q.save_weights('./weights')
 
-    print("\nNumber games won")
+
+#---------- TEST ---------------------
+    
+    count_win = 0
+    count_lose = 0
+    for move in range(number_of_moves):
+        (S, a, r, S_prime) = copy.deepcopy(myFun.play_move(Q, S, rewards_Wi_Lo_Dr_De, epsilon=0))
+        if r == rewards_Wi_Lo_Dr_De[0]:
+            count_win += 1
+        if r == rewards_Wi_Lo_Dr_De[1]:
+            count_lose -= 1
+        # if memory is still no full
+        if len(memory) < memory_size:
+            # add experience to memory
+            memory.append((S,a,r,S_prime))
+        # otherwise we need to empty a slot
+        else:
+            # delete first element
+            memory.pop(0)
+            # add new memory
+            memory.append((S,r,a,S_prime))
+        # S_prime is the next state S
+        S = np.copy(S_prime)
+
+
+    print("\nNumber games won:")
     print(count_win)
-    print("Number games lost")
+    print("Number games lost:")
     print(-count_lose)
     
 
